2f91d1d0c9233af68b81a0234bd8800c9978a3bb
loius_yuyu <j0932854053@gmail.com>
2022-06-08 21:14:01 +0800

    20220608

--- Leetcode/CodeTest/CodeTest/main.c
+++ Leetcode/CodeTest/CodeTest/main.c
@@ -8,45 +8,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* productExceptSelf(int* nums, int numsSize, int* returnSize){
-
-    int prefix = 1;
-    int postfix = 1;
-    *returnSize = numsSize;
-    
-    int *ret = (int* ) malloc(sizeof(int)*numsSize);
-    ret[0] = 1;
-    for (int i = 1; i<numsSize; i++) {
-        prefix = prefix * nums[i-1];
-        ret[i] = prefix ;
-    }
-    for (int i = numsSize-2; i>=0; i--) {
-        postfix = postfix * nums[i+1];
-        ret[i] = ret[i] * postfix;
-    }
-    
-    
-    return ret;
+int cmp(const void *a, const void *b) {
+    int ia = ((int **)a)[0][0];
+    int ib = ((int **)b)[0][0];
+    return (ia - ib);
 }
+
+int eraseOverlapIntervals(int** intervals, int intervalsSize, int* intervalsColSize){
+    int i, count = 0;
+    
+    /* Sort the intervals based on the start time of each intervals */
+    qsort(intervals, intervalsSize, sizeof(intervals[0]), cmp);
+    int end = intervals[0][1];
+
+    for(i = 1; i < intervalsSize; i++) {
+        /* To check the overlap, simply check the end time with the start time of each new interval
+          maintain the end to be minimum of the 2 overlapped interval to minimize the number of erase           */
+        if(end > intervals[i][0]) {
+            //overlap
+            end = end < intervals[i][1]? end:intervals[i][1];
+            count++;
+        } else {
+            /* If there is no overlap, change 'end' to be the current interval's end */
+            end = intervals[i][1];
+        }
+    }
+    return count;
+}
+
+
+
+
  
 int main(int argc, const char * argv[]) {
     
-    int arr[] = {1,2,3,4};
-    int len = sizeof(arr)/sizeof(arr[0]);
-    int *ans;
+    
+    int arr[][2] ={{1,2},{2,3},{3,4},{1,3}};
+    int* ptr[4];
+    ptr[0] = &arr[0][0];
+    ptr[1] = &arr[1][0];
+    ptr[2] = &arr[2][0];
+    ptr[3] = &arr[3][0];
+    
+    int ans;
     int returnsize_temp = 0;
     
-    printf("list = ");
-    for (int i = 0; i<len; i++) {
-        printf("%d ",arr[i]);
-    }
-    ans = productExceptSelf(arr, len, &returnsize_temp);
+    
+    ans = eraseOverlapIntervals(ptr, 4, &returnsize_temp);
+    
+    
+    
     printf("\n");
-    printf("ans  = ");
-    for (int i = 0; i<len; i++) {
-        printf("%d ",ans[i]);
-    }
-    free(ans);
+    printf("ans  = %d",ans);
+    
+    
     printf("\n");
     
     
