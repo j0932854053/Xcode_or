0d77e1afa476cb591f6f04ded308c9709f72450e
loius_yuyu <j0932854053@gmail.com>
2022-06-09 02:00:14 +0800

    20220609

--- Leetcode/Same_Tree/README.md
+++ Leetcode/Same_Tree/README.md
@@ -7,11 +7,11 @@
 
 
 __________________________________________________________
-我自己寫的
+參考網站1建二元樹的資料
 
 
 
-參考網站：
+參考網站1：https://www.geeksforgeeks.org/write-c-code-to-determine-if-two-trees-are-identical/
 
 
 參考網站：
--- Leetcode/Same_Tree/Same_Tree/main.c
+++ Leetcode/Same_Tree/Same_Tree/main.c
@@ -6,6 +6,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 struct TreeNode {
@@ -15,7 +16,15 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
-
+struct TreeNode* newNode(int data)
+{
+    struct TreeNode* TreeNode = (struct TreeNode*) malloc(sizeof(struct TreeNode));
+    TreeNode->val  = data;
+    TreeNode->left  = NULL;
+    TreeNode->right = NULL;
+ 
+    return TreeNode ;
+}
 
 bool isSameTree(struct TreeNode* p, struct TreeNode* q){
 
@@ -30,16 +39,34 @@ bool isSameTree(struct TreeNode* p, struct TreeNode* q){
         return false;
     }
     
-    
-    
     return isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
 }
 
+
+
 int main(int argc, const char * argv[]) {
-    int p[] = {1,2,3};
-    int q[] = {1,2,3};
+    //int p1[3] = {1,2,3};
+    //int q1[3] = {1,2,3};
     
     
+    struct TreeNode *root1 = newNode(1);
+    
+    root1->left = newNode(2);
+    root1->right = newNode(3);
+    root1->left->left  = newNode(4);
+    root1->left->right = newNode(5);
+ 
+    struct TreeNode *root2 = newNode(1);
+    root2->left = newNode(2);
+    root2->right = newNode(3);
+    root2->left->left = newNode(4);
+    root2->left->right = newNode(5);
+ 
+    if(isSameTree(root1, root2))
+        printf("Both tree are identical.");
+    else
+        printf("Trees are not identical.");
+ 
     printf("\n");
     return 0;
 }
