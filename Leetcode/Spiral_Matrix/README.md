6b691d1289ed02fc9a4d864d579e0a5a65912287
loius_yuyu <j0932854053@gmail.com>
2022-06-10 04:58:34 +0800

    20220610

---
+++ Leetcode/Spiral_Matrix/README.md
@@ -0,0 +1,12 @@
+# New Document
+
+
+``` 
+
+
+__________________________________________________________
+
+
+
+
+參考網站：https://leetcode.com/problems/spiral-matrix/discuss/20702/C-Solution
---
+++ Leetcode/Spiral_Matrix/Spiral_Matrix/main.c
@@ -0,0 +1,141 @@
+//
+//  main.c
+//  Spiral_Matrix
+//
+//  Created by 龔星宇 on 2022/6/10.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize){
+
+    if(0 == matrixSize)
+        {
+            *returnSize = 0;
+            return NULL;
+        }
+    
+    
+    * returnSize = matrixSize * (*matrixColSize);
+    int *ret = (int* ) malloc(sizeof(int) * matrixSize * (*matrixColSize));
+    
+    int up = 0;
+    int down = matrixSize;
+    int left = 0;
+    int right = (* matrixColSize);
+    int counter = 0;
+    
+    while ((up<=down) && (up<=down)) {
+        if (up<=down) {
+            for (int i = left; i<right; i++) {
+                ret[counter] = matrix[up][i];
+                counter++;
+            }
+            up++;
+        }
+    
+        
+        if (left<=right) {
+            for (int i = up; i<down; i++) {
+                ret[counter] = matrix[i][right-1];
+                counter++;
+            }
+            right--;
+        }
+        
+        
+        if (up<=down) {
+            for (int i = right-1; i>=left; i--) {
+                ret[counter] = matrix[down-1][i];
+                counter++;
+            }
+            down--;
+        }
+        
+        
+        if (left<=right) {
+            for (int i = down-1; i>=up; i--) {
+                ret[counter] = matrix[i][left];
+                counter++;
+            }
+            left++;
+        }
+        
+        
+    }
+    /*
+    while (1) {
+        if (up<=down) {
+            for (int i = left; i<right; i++) {
+                ret[counter] = matrix[up][i];
+                counter++;
+            }
+            up++;
+        }
+        else{
+            break;
+        }
+        if (left<=right) {
+            for (int i = up; i<down; i++) {
+                ret[counter] = matrix[i][right-1];
+                counter++;
+            }
+            right--;
+        }
+        else{
+            break;
+        }
+        if (up<=down) {
+            for (int i = right-1; i>=left; i--) {
+                ret[counter] = matrix[down-1][i];
+                counter++;
+            }
+            down--;
+        }
+        else{
+            break;
+        }
+        if (left<=right) {
+            for (int i = down-1; i>=up; i--) {
+                ret[counter] = matrix[i][left];
+                counter++;
+            }
+            left++;
+        }
+        else{
+            break;
+        }
+        
+    }
+    */
+    
+    return ret;
+}
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    //int matrix_test[][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int matrix_test[][5] = {{1,2,3,4,11},{5,6,7,8,22},{9,10,11,12,32}};
+    //int matrix_test[][2] = {{1,2},{4,5},{7,8}};
+    int* ptr[3];
+    ptr[0] = &matrix_test[0][0];
+    ptr[1] = &matrix_test[1][0];
+    ptr[2] = &matrix_test[2][0];
+    int matrixColSize_temp = 5;
+    int returnSiz_temp;
+   
+    int* ans = spiralOrder(ptr, 3, &matrixColSize_temp, &returnSiz_temp);
+    
+   
+    
+    for (int i = 0; i<returnSiz_temp; i++) {
+        printf("%d ",ans[i]);
+    }
+    
+    free(ans);
+    printf("\n");
+    return 0;
+}
