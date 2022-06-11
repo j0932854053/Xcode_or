c1a4018961e1be0277431a18a4f849c53d678e4f
loius_yuyu <j0932854053@gmail.com>
2022-06-11 22:16:53 +0800

    202220611

--- Leetcode/Jump_Game/Jump_Game/main.c
+++ Leetcode/Jump_Game/Jump_Game/main.c
@@ -10,39 +10,47 @@
 
 bool canJump(int* nums, int numsSize){
 
-    int index = 0;
+    int maxindex = nums[0]; // 當前能夠跳躍到的最遠位置
+    int nextmax = 0;
+    int current = 0;    // 當下在的位置
     
-    if(numsSize==1) {
-        return true;
-    }
-    
-    
-    if(nums[0]==0 && (numsSize==1)) {
-        return true;
-        
-    }
-
-    
-    while (index<numsSize-1) {
-        if (nums[index]==0 ) {
+    // 最遠位置小於陣列的終點位置時 持續尋找
+    while (maxindex<numsSize-1) {
+        // 下一步可以跳得最遠距離
+        nextmax = maxindex;
+        //  循環當下位置可以跳躍的區間中最遠可以跳到哪裡
+        for (int i = current+1 ; i<=maxindex; i++) {
+            
+            nextmax = nextmax > (i + nums[i]) ? nextmax : (i + nums[i]);
+        }
+        // 如果下一步的最遠位置不能超越當前所能跳躍的最遠位置 直接回傳false
+        if (nextmax<=maxindex) {
             return false;
         }
-        index = nums[index]+index;
-        if (index>=numsSize-1) {
-            return true;
-        }
+        // 當前位置更新為原本可以跳躍到的最遠位置
+        current = maxindex;
+        //  最遠位置更新為原本下一步可以到達的最遠位置
+        maxindex = nextmax;
     }
     
     
     
-    return 0;
+    return true;
 }
 
 
 
 int main(int argc, const char * argv[]) {
     
+    int nums[] = {2,5,0,0};
+    int len = sizeof(nums)/sizeof(nums[0]);
+    bool ans = canJump(nums, len);
     
+    if (ans) {
+        printf("true");
+    }else{
+        printf("false");
+    }
     
     
     
--- Leetcode/Jump_Game/README.md
+++ Leetcode/Jump_Game/README.md
@@ -1,12 +1,58 @@
 
+55.Jump Game
 
+Medium
+
+
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+``` 
+Input: nums = [2,3,1,1,4]
+Output: true
+``` 
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+``` 
+Input: nums = [3,2,1,0,4]
+Output: false
+``` 
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ 
+
+Constraints:
+``` 
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
 
 ``` 
 
 
 __________________________________________________________
 我看過最屌的解法
+``` c
+/* 贪心算法：
+* 一次遍历，在遍历过程中，判断基于当前位置的最远距离是否到达最后一个元素，
+* 如果到达或者越过，则直接返回真; 否则假。
+*/
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-
-
+bool canJump(int* nums, int numsSize){
+    int i;
+    int max = 0;
+    for (i = 0; i < numsSize; i++) {
+        if (i <= max) {
+            max = MAX(max, nums[i] + i);
+            if (max >= numsSize - 1)
+                return true;
+        }
+    }
+    
+    return false;
+}
+``` 
 參考網站：https://leetcode.jp/leetcode-55-jump-game-%E8%A7%A3%E9%A2%98%E6%80%9D%E8%B7%AF%E5%88%86%E6%9E%90/
